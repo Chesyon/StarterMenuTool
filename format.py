524db241767cc2cd3a2c92e02d1d5a309f1e14b0
Chesyon <55322011+Chesyon@users.noreply.github.com>
2024-05-26 14:35:30 -0400

    New version (better)

--- format.py
+++ format.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+import csv
+import textwrap
 
 
 @dataclass
@@ -6,11 +8,12 @@ class Pokemon:
     name: str
     species: int
     gender: int
+    talk_kind: int
 
 
 def gen_starter_option(species_name: str, species_id: int, gender: int, is_partner: int, starter_setter_sp: int,
                        set_frame_color_sp: int, choose_gender: bool, gender_list: int, confirm_species: bool,
-                       confirmation_string: str) -> str:
+                       confirmation_string: str, talk_kind: int) -> str:
     confirmation_string = confirmation_string.replace("[SPECIESNAME]", "[CS:K]" + species_name + "[CR]")
     indent_if_confirm = ""
     if confirm_species:
@@ -26,7 +29,12 @@ def gen_starter_option(species_name: str, species_id: int, gender: int, is_partn
     if confirm_species:
         output += "          message_Monologue('" + confirmation_string + "');\n          switch ( message_SwitchMenu(2, 1) ) {\n               case menu('Yes'):\n                    message_Close();\n"
     output += "               " + indent_if_confirm + "ProcessSpecial(" + str(starter_setter_sp) + "," + str(
-        is_partner) + "," + str(species_id) + ");\n"
+        is_partner) + "," + str(species_id) + ");\n" + "               " + indent_if_confirm + "$"
+    if is_partner == 0:
+        output += "HERO"
+    else:
+        output += "PARTNER"
+    output += "_TALK_KIND = " + str(talk_kind) + ";\n"
     if not choose_gender and not is_partner:
         output += "               " + indent_if_confirm + "ProcessSpecial(" + str(set_frame_color_sp) + "," + str(
             gender) + ",0);\n"
@@ -67,7 +75,8 @@ def gen_menu_footer(choose_gender: bool, gender_list: int, is_partner: int):
 
 
 def gen_menu(gender_prompt: str, species_prompt: str, species_confirmation: str, confirm_species: bool,
-             choose_gender: bool, is_partner: int, mons: list, female_mons: list, starter_setter_sp: int, set_frame_color_sp: int):
+             choose_gender: bool, is_partner: int, mons: list, female_mons: list, starter_setter_sp: int,
+             set_frame_color_sp: int):
     output = ""
     if choose_gender:
         output += gen_gender_prompt(is_partner, gender_prompt, set_frame_color_sp)
@@ -75,7 +84,8 @@ def gen_menu(gender_prompt: str, species_prompt: str, species_confirmation: str,
     i = 0
     while i != len(mons):
         output += gen_starter_option(mons[i].name, mons[i].species, mons[i].gender, is_partner, starter_setter_sp,
-                                     set_frame_color_sp, choose_gender, 0, confirm_species, species_confirmation)
+                                     set_frame_color_sp, choose_gender, 0, confirm_species, species_confirmation,
+                                     mons[i].talk_kind)
         i += 1
     output += gen_menu_footer(choose_gender, 0, is_partner)
     if choose_gender:
@@ -84,19 +94,108 @@ def gen_menu(gender_prompt: str, species_prompt: str, species_confirmation: str,
         while i != len(female_mons):
             output += gen_starter_option(female_mons[i].name, female_mons[i].species, female_mons[i].gender, is_partner,
                                          starter_setter_sp, set_frame_color_sp, choose_gender, 1, confirm_species,
-                                         species_confirmation)
+                                         species_confirmation, female_mons[i].talk_kind)
             i += 1
         output += gen_menu_footer(choose_gender, 1, is_partner)
     return output
 
 
-# PLAYER SETTINGS
-player_pokemon = [Pokemon("Torchic", 283, 0), Pokemon("Charmander", 4, 0), Pokemon("Pikachu", 25, 0), Pokemon("Totodile", 158, 0), Pokemon("Piplup", 428, 0), Pokemon("Chimchar", 425, 0), Pokemon("Cyndaquil", 155, 0), Pokemon("Shinx", 438, 0), Pokemon("Riolu", 489, 0), Pokemon("Chikorita", 152, 0), Pokemon("Phanpy", 258, 0), Pokemon("Bulbasaur", 1, 0), Pokemon("Squirtle", 7, 0), Pokemon("Treecko", 280, 0), Pokemon("Mudkip", 286, 0), Pokemon("Turtwig", 422, 0)]
-player_pokemon_female = [Pokemon("Treecko", 880, 1), Pokemon("Bulbasaur", 4, 601), Pokemon("Charmander", 25, 604), Pokemon("Eevee", 733, 1), Pokemon("Chimchar", 1025, 1), Pokemon("Skitty", 928, 1), Pokemon("Turtwig", 1022, 1), Pokemon("Pikachu", 625, 1), Pokemon("Totodile", 758, 1), Pokemon("Cyndaquil", 755, 1), Pokemon("Vulpix", 637, 1), Pokemon("Mudkip", 886, 1), Pokemon("Piplup", 1028, 1), Pokemon("Chikorita", 752, 1), Pokemon("Torchic", 883, 1), Pokemon("Squirtle", 607, 1)]
+def parse_csv_settings(is_partner: int):
+    # this should output: gender_prompt, species_prompt, species_confirmation, confirm_species, choose_gender, is_partner, mons, female_mons, starter_setter_sp, set_frame_color_sp
+    # row_num 0 is 1
+    # col_num 0 is A
+    # accessing a cell can be done as mycsv[row_num][col_num]
+    if is_partner != 0 and is_partner != 1:
+        raise ValueError("is_partner must be 0 or 1!")
+    with open('settings.csv', 'r') as f:
+        mycsv = csv.reader(f)
+        mycsv = list(mycsv)
+        mycsv.append(['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''])
+        if is_partner == 0:
+            species_prompt = mycsv[1][1]
+        else:
+            species_prompt = mycsv[4][1]
+        if species_prompt == '':
+            raise ValueError("Species prompt cannot be empty!")
+        if is_partner == 0:
+            species_confirmation = mycsv[2][1]
+        else:
+            species_confirmation = mycsv[5][1]
+        if species_confirmation == '':
+            confirm_species = False
+        else:
+            confirm_species = True
+        if is_partner == 0:
+            gender_prompt = mycsv[0][1]
+        else:
+            gender_prompt = mycsv[3][1]
+        if gender_prompt == '':
+            choose_gender = False
+        else:
+            choose_gender = True
+        if is_partner == 0:
+            first_col = 3
+        else:
+            first_col = 13
+        cur_row = 2
+        mons = []
+        while is_mon_valid(mycsv[cur_row][first_col], mycsv[cur_row][first_col + 1], mycsv[cur_row][first_col + 2],
+                           mycsv[cur_row][first_col + 3]):
+            mons.append(Pokemon(mycsv[cur_row][first_col], int(mycsv[cur_row][first_col + 1]),
+                                int(mycsv[cur_row][first_col + 2]), int(mycsv[cur_row][first_col + 3])))
+            cur_row += 1
+        if choose_gender == 1:
+            if is_partner == 0:
+                first_col = 8
+            else:
+                first_col = 18
+            cur_row = 2
+            female_mons = []
+            while is_mon_valid(mycsv[cur_row][first_col], mycsv[cur_row][first_col + 1], mycsv[cur_row][first_col + 2],
+                               mycsv[cur_row][first_col + 3]):
+                female_mons.append(Pokemon(mycsv[cur_row][first_col], int(mycsv[cur_row][first_col + 1]),
+                                           int(mycsv[cur_row][first_col + 2]), int(mycsv[cur_row][first_col + 3])))
+                cur_row += 1
+        else:
+            female_mons = []
+        starter_setter_sp = int(mycsv[11][1])
+        set_frame_color_sp = int(mycsv[12][1])
+    return [gender_prompt, species_prompt, species_confirmation, confirm_species, choose_gender, is_partner, mons,
+            female_mons, starter_setter_sp, set_frame_color_sp]
 
-# PARTNER SETTINGS
-partner_pokemon = [Pokemon("Bulbasaur", 1, 0), Pokemon("Charmander", 4, 0), Pokemon("Squirtle", 7, 0), Pokemon("Pikachu", 25, 0), Pokemon("Chikorita", 752, 1), Pokemon("Cyndaquil", 155, 0), Pokemon("Totodile", 158, 0), Pokemon("Treecko", 280, 0), Pokemon("Torchic", 883, 1), Pokemon("Mudkip", 286, 0), Pokemon("Turtwig", 422, 0),Pokemon("Chimchar", 425, 0), Pokemon("Piplup", 428, 0), Pokemon("Eevee", 733, 1), Pokemon("Shinx", 438, 0), Pokemon("Riolu", 489, 0), Pokemon("Phanpy", 258, 0), Pokemon("Vulpix", 637, 1), Pokemon("Skitty", 928, 1), Pokemon("Meowth", 52, 0), Pokemon("Munchlax", 488, 0)]
-partner_pokemon_female = partner_pokemon
 
-print(gen_menu("Are you male or female?", "Who would you like to be?", "Is [SPECIESNAME] who you want?", True, True, 0, player_pokemon, player_pokemon_female, 64, 65))
-print(gen_menu("Are you male or female?", "Choose the Pokémon you want for a partner.", "Is [SPECIESNAME] who you want?", True, False, 1, partner_pokemon, partner_pokemon_female, 64, 65))
+def is_mon_valid(name: str, species: str, gender: str, talk_kind: str):
+    if name == '' or species == '' or gender == '' or talk_kind == '':
+        return False
+    else:
+        try:
+            int(species)
+            int(gender)
+            int(talk_kind)
+        except ValueError:
+            return False
+    return True
+
+
+def gen_script():
+    output = ""
+    with open('script_header.txt', 'r') as f:
+        output += f.read()
+    output += "\n"
+    hero_settings = parse_csv_settings(0)
+    output += textwrap.indent(
+        gen_menu(hero_settings[0], hero_settings[1], hero_settings[2], hero_settings[3], hero_settings[4],
+                 hero_settings[5], hero_settings[6], hero_settings[7], hero_settings[8], hero_settings[9]), '     ')
+    partner_settings = parse_csv_settings(1)
+    output += textwrap.indent(
+        gen_menu(partner_settings[0], partner_settings[1], partner_settings[2], partner_settings[3],
+                 partner_settings[4], partner_settings[5], partner_settings[6], partner_settings[7],
+                 partner_settings[8], partner_settings[9]), '     ')
+    output += "\n"
+    with open('script_footer.txt', 'r') as f:
+        output += f.read()
+    return output
+
+
+with open("output.txt", "w") as file:
+    file.write(gen_script())
